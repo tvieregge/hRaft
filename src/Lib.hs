5caa0dc0c6a77dbf87031f311e0458f7caecd385
tvieregge <tmvieregge@gmail.com>
2019-03-04 00:41:16 -0500

    wip, can start and find multiple nodes

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,32 +21,36 @@ echo = do
 someFunc :: IO ()
 someFunc = do
   -- Right t <- createTransport "127.0.0.1" "10501" (\p -> ("","")) defaultTCPParameters
-  backend <- initializeBackend "127.0.0.1" "10515" initRemoteTable
+  backend <- initializeBackend "127.0.0.1" "10518" initRemoteTable
   nodes <- replicateM 3 $ newLocalNode backend
   node <- newLocalNode backend
-  remotes <- mapM (flip forkProcess echo) nodes
-  runProcess node $ do
-    -- Spawn another worker on the local node
-    echoPid <- spawnLocal $ forever $ echo
-
-    -- The `say` function sends a message to a process registered as "logger".
-    -- By default, this process simply loops through its mailbox and sends
-    -- any received log message strings it finds to stderr.
-
-    say "send some messages!"
-    send echoPid "hello"
-    mapM ((flip send) "hello2") remotes
-    self <- getSelfPid
-    mapM (flip send (self, "hello")) remotes
-    send echoPid (self, "hello")
-
-    -- `expectTimeout` waits for a message or times out after "delay"
-    m <- expectTimeout 1000000
-    case m of
-      -- Die immediately - throws a ProcessExitException with the given reason.
-      Nothing  -> die "nothing came back!"
-      Just s -> say $ "got " ++ s ++ " back!"
-
-    -- Without the following delay, the process sometimes exits before the messages are exchanged.
+  -- remotes <- mapM (flip forkProcess echo) nodes
+  runProcess node $ forever $ do
+    peers <- liftIO $ findPeers backend 1000000
+    liftIO . print $  show peers
     liftIO $ threadDelay 2000000
+    return ()
+    -- -- Spawn another worker on the local node
+    -- echoPid <- spawnLocal $ forever $ echo
+
+    -- -- The `say` function sends a message to a process registered as "logger".
+    -- -- By default, this process simply loops through its mailbox and sends
+    -- -- any received log message strings it finds to stderr.
+
+    -- say "send some messages!"
+    -- send echoPid "hello"
+    -- mapM ((flip send) "hello2") remotes
+    -- self <- getSelfPid
+    -- mapM (flip send (self, "hello")) remotes
+    -- send echoPid (self, "hello")
+
+    -- -- `expectTimeout` waits for a message or times out after "delay"
+    -- m <- expectTimeout 1000000
+    -- case m of
+    --   -- Die immediately - throws a ProcessExitException with the given reason.
+    --   Nothing  -> die "nothing came back!"
+    --   Just s -> say $ "got " ++ s ++ " back!"
+
+    -- -- Without the following delay, the process sometimes exits before the messages are exchanged.
+    -- liftIO $ threadDelay 2000000
   -- mapM_ closeLocalNode nodes
